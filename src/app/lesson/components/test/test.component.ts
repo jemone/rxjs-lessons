cf47d52d4ad45e3b91872e0d2d81e42576b848f8
Jemone S <es.newone@egmail.com>
2021-06-20 22:51:52 +0300

    Observable subscriber test

--- src/app/lesson/components/test/test.component.ts
+++ src/app/lesson/components/test/test.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'mc-test',
@@ -6,7 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.scss'],
 })
 export class TestComponent implements OnInit {
+  myForm: FormGroup;
+
+  // search$ = new Observable((observer) => {
+  //   console.log('Start in observable');
+  //   const myFiled = this.myForm.controls['controlSearch']
+  //   console.log(myFiled.value);
+  //     // console.log(this.myForm.value);
+  //     // myFiled?.addEventListener('input', (event) => {
+  //     //   observer.next(event);
+  //     });
+  //     console.log('End in observable');
+  //   });
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      controlSearch: '',
+    });
+  }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    console.log('Start subscribe');
+
+    // this.search$.subscribe((value) => {
+    //   console.log(value);
+    // });
+
+    console.log('End subscribe');
+  }
+
+  onSubmit(): void {
+    console.log(this.myForm.value);
+    console.log(this.myForm.valid);
+    console.log(this.myForm.controls['controlSearch'].value);
+
+    const myFiled = this.myForm.controls['controlSearch'];
+    console.log(myFiled.value);
   }
 }
--- src/app/lesson/lesson.module.ts
+++ src/app/lesson/lesson.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { compileDeclareNgModuleFromMetadata } from '@angular/compiler';
 import { Component, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from './components/test/test.component';
 
@@ -12,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(routes)],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule.forRoot(routes)],
   declarations: [TestComponent],
 })
 export class LessonModule {}
